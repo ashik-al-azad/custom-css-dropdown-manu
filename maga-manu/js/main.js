8202f37752afed6508c6fe93443c11bf9ef1e399
Ashik Al Azad <83364890+ashik-al-azad@users.noreply.github.com>
2022-11-21 17:55:16 +0600

    Add files via upload

---
+++ maga-manu/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".manu-icon").on("click", function () {
+        $("#menu").toggleClass("show");
+    });
+});
+
+// toogle
+// const chk = document.getElementById("chk");
+
+// chk.addEventListener("change", () => {
+//     document.body.classList.toggle("dark");
+// });
